aaed59ec3d9fab9f16e47945d3a4a36b4e1b9a59
Jon Rigert <jon.rigert@hs2solutions.com>
2015-07-24 12:49:09 -0400

    Convert JSON data if it doesn't exist when doing a GET

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ var express = require('express'), // Node Express Module
     fs = require('fs'); // File Stream
 
 var port = 8000;
+var fileNames = {
+    csv: './server/marriott-data.csv',
+    json: './server/marriott-data.json'
+};
 
 // Initialize the Express App
 var app = express();
@@ -23,30 +27,67 @@ app.get('/', function(request, response){
     response.sendfile(path.join(__dirname, '../public/index.html'));
 });
 
-app.post('/convert', function(request, response){
-    var csvStream = fs.createReadStream('./server/marriott-data.csv');
-    var jsonStream = fs.createWriteStream('./server/marriott-data.json');
+// Convert CSV file to JSON file
+function convertData(callback){
+    var csvStream = fs.createReadStream(fileNames.csv);
+    var jsonStream = fs.createWriteStream(fileNames.json);
 
     var csvConverter = new Converter({constructResult:false, toArrayString:true});
 
     csvConverter.on('end_parsed', function(){
         console.log('CSV Data successfully converted!');
-
-        response.send('CSV Data successfully converted!');
+        callback();
     });
 
     csvStream.pipe(csvConverter).pipe(jsonStream);
+}
+
+function readJsonData(callback){
+    fs.readFile(fileNames.json, function(err, data){
+        if (err){
+            callback(err, null);
+        }
+        else {
+            callback(null, data);
+        }
+    });
+}
+
+app.post('/convert', function(request, response){
+    convertData(function(){
+        response.send('CSV Data successfully converted!');
+    });
 });
 
 app.get('/breaks', function(request, response){
-    fs.readFile('./server/marriott-data.json', function(err, data){
+    // Check if JSON file exists. If not, convert the csv first
+    fs.stat(fileNames.json, function(err, stat){
+        if (!err){
+            // No error, file exists. Read and send
+            readJsonData(sendJsonData);
+        }
+        else if (err.code === 'ENOENT') {
+            // File doesn't exist. Convert and send
+            convertData(function(){
+                readJsonData(sendJsonData);
+            });
+        }
+        else {
+            // Unknown error
+            response.status(500).send('Error reading eBreaks data: ' + err.code);
+        }
+
+    });
+
+    function sendJsonData(err, data){
         if (err){
             response.status(500).send('Error reading eBreaks data');
         }
         else {
             response.send(JSON.parse(data));
         }
-    });
+    }
+
 });
 
 // Start the server
