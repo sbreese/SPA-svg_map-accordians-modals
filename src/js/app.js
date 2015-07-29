3d11df172194b0455d84342ce735d04dc600d8ee
Jon Rigert <jon.rigert@hs2solutions.com>
2015-07-29 10:00:27 -0400

    A little more smooth of a scroll

--- src/js/app.js
+++ src/js/app.js
@@ -14,4 +14,5 @@ config(['$routeProvider', function ($routeProvider) {
 }]).
 
  // Setup scroll easing for duScroll directive
-value('duScrollEasing', function (t) { return t<0.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1; });
+value('duScrollEasing', function (t) { return t<0.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1; }).
+value('duScrollDuration', 700);
